d3e45bf0b1988fa78ceb35760c2a6b4068b839d1
Dr. Azrael Tod <dat-git@g33ky.de>
2012-11-05 21:41:58 +0100

    m3u-indexer commited, vim-trash deleted

---
+++ shell/m3u.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+dir="$1"
+#echo "Create playlist for $1 ..."
+if [[ $2 ]]; then list="$2"; else list="$1"; fi
+ 
+pushd "$dir" 2>&1 >/dev/null
+#find . -type f -name "*.mp3" -or -name "*.ogg" -or -name "*.m4a" -or -name "*.opus" -or -name "*.avi" > "$list.m3u"
+find . -type f \
+	-not -name "*.m3u" \
+	-and -not -name "*.asx" \
+	-and -not -name "done" \
+	-and -not -name "errors" \
+	-and -not -name "*.html" \
+	-and -not -name "*.js" \
+	-and -not -name "*.swp" \
+	> "$list.m3u"
+wc -l "$list.m3u"
+popd 2>&1 >/dev/null
