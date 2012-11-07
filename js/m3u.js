f58a3f28e30d29c8cd742037712b8af9d9a70b3d
Dr. Azrael Tod <dat-git@g33ky.de>
2012-11-07 07:34:09 +0100

    autoplay

--- js/m3u.js
+++ js/m3u.js
@@ -29,11 +29,16 @@ var m3u = function(targetID, m3uPath, sequential) {
 		var text = '<ol class="playlist">'
 		for (var j=0;j<files.length;j++) {
 			if (files[j]) {
-				var name = this.getFilename(j);
+				var feed = this.getFeedname(j);
+				var inner;
+				if (feed) {
+					inner = '<span class="feed">'+feed+'</span>';
+				}
+				inner += '<span class="episode">'+this.getFilename(j)+'</span>';
 				if (i==j) {
-					text += '<li class="active">'+name+'</li>';
+					text += '<li class="active">'+inner+'</li>';
 				} else {
-					text += '<li class="inactive" onclick="m3u.gotoFile('+(j)+')">'+name+'</li>';
+					text += '<li class="inactive" onclick="m3u.gotoFile('+(j)+')">'+inner+'</li>';
 				}
 			}
 		}
@@ -59,11 +64,23 @@ var m3u = function(targetID, m3uPath, sequential) {
 		this.cleanupFiles();
 		var target = document.getElementById(targetID);
                 target.innerHTML = this.drawOne(this.current_file)+this.drawControls(this.current_file);
+		var player = document.getElementById('audio_'+i);
+		if (player) {
+			if (i < files.length) {
+				player.addEventListener('ended', function() {m3u.gotoFile(i)});
+			}
+			player.play();
+		}
 	}
 	m3u.getFilename = function(i) {
 		var parts = files[i].split('/');
                 return parts[parts.length-1];
 	}
+	m3u.getFeedname = function(i) {
+                var parts = files[i].split('/');
+		if (parts.length < 2) return false;
+                return parts[parts.length-2];
+        }
 	m3u.drawOne = function(i) {
 
 		var url = files[i];
@@ -75,7 +92,7 @@ var m3u = function(targetID, m3uPath, sequential) {
 //                      }
                         var type = this.testURL(url);
                         if(type) {
-                        	return this.getPlayerText(url, name, type);
+                        	return this.getPlayerText(url, name, type, i);
                         } else {
 				files[i] = false;
 				if (i<files.length-1) {
@@ -93,7 +110,7 @@ var m3u = function(targetID, m3uPath, sequential) {
 		return text;
 	}
 	m3u.getPlayerText = function(url, name, type, id) {
-		var text = '<audio name="audio_'+id+'" controls="controls">';
+		var text = '<audio id="audio_'+id+'" controls="controls">';
 		text += '<source src="'+url+'" type="'+type+'" ';
 		text += 'preload="metadata" /></audio>';
 		text += '<label><a href="'+url+'" target="_blank">';
