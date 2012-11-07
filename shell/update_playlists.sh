ca263b8a1714ea1b42392fb360b266b32d14b05b
Dr. Azrael Tod <dat-git@g33ky.de>
2012-11-07 19:13:32 +0100

    einzelne Podcasts umbenannt

--- shell/update_playlists.sh
+++ shell/update_playlists.sh
@@ -2,6 +2,10 @@
 pushd .;
 cd /home/dat/AeroFS/public/podcasts;
 rm ./*.m3u
+mv Comedy/Neues\ vom\ K\&#xE4nguru/ Comedy/Neues\ vom\ Kaenguru;
+mv ÖR/Klassiker\ von\ \&quotFragen\ an\ den\ Autor\&quot ÖR/Klassiker\ von\ Fragen\ an\ den\ Autor;
+mv sonst/ZEIT\ Wissen\ \&#x2013\ Der\ Podcast/ sonst/ZEIT\ Wissen
+mv musik/AVICII\ -\ LEVELS\ PODCAST/ musik/AVICII
 m3u.sh . relative;
 sed -e "s/.\//http:\/\/g33ky.de\/static\/podcasts\//" relative.m3u > absolute.m3u;
 cat absolute.m3u;
