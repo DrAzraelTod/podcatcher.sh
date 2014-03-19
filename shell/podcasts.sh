9ee726634c43ab5e352ea066f05e6b16107c16f3
Dr. Azrael Tod <code@g33ky.de>
2014-03-19 08:50:06 +0100

    minimal changes

--- shell/podcasts.sh
+++ shell/podcasts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 podget --verbosity 1 >&1;
 f=`dirname "$0"`;
-$f/update_playlists.sh >/dev/null;
+update_playlists.sh >/dev/null;
--- shell/update_playlists.sh
+++ shell/update_playlists.sh
@@ -2,7 +2,7 @@
 clear;
 f=`dirname "$0"`;
 pushd .;
-cd /home/dat/AeroFS/public/podcasts;
+cd /home/webchao/web/podcasts;
 rm ./*.m3u
 #mv Comedy/Neues\ vom\ K\&#xE4nguru/ Comedy/Neues\ vom\ Kaenguru;
 #mv Comedy/NDR\ 2\ -\ Fr\&#xFChst\&#xFCck\ bei\ Stefanie Comedy/Fruehstueck\ bei\ Stefanie
@@ -11,8 +11,10 @@ rm ./*.m3u
 #mv musik/AVICII\ -\ LEVELS\ PODCAST/ musik/AVICII
 #mv ÖR/IQ\ -\ Wissenschaft\ und\ Forschung\ -\ Bayern\ 2 ÖR/IQ\ -\ Wissenschaft\ und\ Forschung
 $f/m3u.sh . relative;
-#tar -pczf export.tgz sonst musik ccc ÖR game Comedy relative.m3u
+#tar -pczf export.tgz sonst musik ccc game Comedy Labertaschen Politik Thema mini tech relative.m3u
 rmdir ./*/* 2>/dev/null;
+rmdir ./* 2>/dev/null;
+echo 'Verzeichnisse reduziert';
 sed -e "s/.\//http:\/\/g33ky.de\/static\/podcasts\//" relative.m3u > absolute.m3u;
 cat absolute.m3u;
 echo  -n 'Gefundene feeds:';
