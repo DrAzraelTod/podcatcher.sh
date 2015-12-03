8699f6d4c14ecd1ddce5ff1834282b7c3af1628e
Dr. Azrael Tod <dat@v22015041679024799.yourvserver.net>
2015-12-03 12:13:05 +0100

    m3u.sh now excludes empty/hidden files and no longer adds .m3u to the filename

--- shell/m3u.sh
+++ shell/m3u.sh
@@ -4,7 +4,6 @@ dir="$1"
 if [[ $2 ]]; then list="$2"; else list="$1"; fi
  
 pushd "$dir" 2>&1 >/dev/null
-#find . -type f -name "*.mp3" -or -name "*.ogg" -or -name "*.m4a" -or -name "*.opus" -or -name "*.avi" > "$list.m3u"
 find . -type f \
 	-not -name "*.m3u" \
 	-and -not -name "*.asx" \
@@ -17,7 +16,9 @@ find . -type f \
 	-and -not -name "*.sh" \
 	-and -not -name "*.md" \
 	-and -not -name "*.tgz" \
-	> "$list.m3u" -exec ls -1rt "{}" +;
-wc -l "$list.m3u"
-sed -i 's/ /%20/g' "$list.m3u"
+        -and -not -name ".*" \
+        -and -not -empty \
+	> "$list" -exec ls -1rt "{}" +;
+wc -l "$list"
+sed -i 's/ /%20/g' "$list"
 popd 2>&1 >/dev/null
